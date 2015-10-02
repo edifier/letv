dbee43521f2b636b84b666ae1e673a1e5e6558f2
王鑫 <wangxin@wangxindeMBP.lan>
2015-10-02 12:44:48 +0800

    增加npm支持

---
+++ pay/src/rjs/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wangxin on 15/10/2.
+ */
+var trace = require('../../server/proxy/trace.js');
+
+module.exports = function(){
+    console.log('这是一个测试文件');
+    console.log('rjs文件夹下可以用*.js文件');
+    console.log('js文件下使用*_rjs.js文件');
+    console.log('测试watch功能，OK了！');
+};
